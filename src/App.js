0f936123f8360a0992ca6045890c22dd8d6a6bf6
Fatima Mohamedali <fatimamohamedali@fatimas-mbp.myfiosgateway.com>
2022-03-23 12:54:35 -0400

    initialized project and added header, banner fields

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Home from './Home';
+import Header from './Header';
+import Banner from './Banner'
+
+function App() {
+  return (
+    // BEM
+    <div className="app">
+      <Header />
+      {/* Home */}
+      <Home />
+      <Banner />
+          {/* Header */}
+
+          {/* Banner */}
+            {/* Search */}
+
+          {/* Cards */}
+
+          {/* Footer */}
+
+      {/* Search Page */}
+    </div>
+  );
+}
+
+export default App;
