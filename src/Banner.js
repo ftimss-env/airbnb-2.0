a8ea97c82af7388891d9d75b16a5e9c559fef6d8
Fatima Mohamedali <fatimamohamedali@fatimas-mbp.myfiosgateway.com>
2022-03-29 13:27:26 -0400

    Added routing to logo, explore nearby, and search airbnb to search page

--- src/Banner.js
+++ src/Banner.js
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 import "./Banner.css";
 import { Button } from "@material-ui/core";
-import Search from './Search'
+import Search from "./Search";
+import { useHistory } from "react-router-dom";
 
 function Banner() {
+  const history = useHistory();
   const [showSearch, setShowSearch] = useState(false);
 
   return (
     <div className="banner">
       <div className="banner__search">
-          {showSearch && <Search />}
-        <Button onClick={() => setShowSearch(!showSearch)} className="banner__searchButton" variant="outlined">
+        {showSearch && <Search />}
+        <Button
+          onClick={() => setShowSearch(!showSearch)}
+          className="banner__searchButton"
+          variant="outlined"
+        >
           {showSearch ? "Hide" : "Search Dates"}
         </Button>
       </div>
@@ -19,7 +25,7 @@ function Banner() {
         <h5>
           Plan a different kind of getaway to uncover the hidden gems near you.
         </h5>
-        <Button variant="outlined"> Explore Nearby</Button>
+        <Button onClick={()=> history.push('/search')} variant="outlined"> Explore Nearby</Button>
       </div>
     </div>
   );
--- src/Search.js
+++ src/Search.js
@@ -5,9 +5,10 @@ import 'react-date-range/dist/theme/default.css';
 import {DateRangePicker} from 'react-date-range';
 import PeopleIcon from '@material-ui/icons/People'
 import {Button} from '@material-ui/core'
+import {useHistory} from 'react-router-dom'
 
 function Search() {
-
+    const history = useHistory();
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
 
@@ -29,7 +30,7 @@ function Search() {
         <input min={0}
         defaultValue={2}
         type='number'/>
-        <Button>Search Airbnb</Button>
+        <Button onClick={()=> history.push('/search')}>Search Airbnb</Button>
     </div>
   )
 }
